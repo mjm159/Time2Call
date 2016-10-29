6e5cf08551eeb24396b5cb1a09bff71259324ae9
Michael Malocha <mjmaloch@us.ibm.com>
2016-10-29 16:23:28 -0400

    remove unnecessary modules

--- sqlalchemy_declarative.py
+++ sqlalchemy_declarative.py
@@ -2,7 +2,6 @@ import os
 import sys
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
